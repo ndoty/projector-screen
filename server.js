3b47c13542471ef4e5047c2350e01a7e0dcee80d
Nick Faust-Doty <n.faustdoty@gmail.com>
2016-01-11 22:24:20 -0500

    gpio beginnings

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 var http = require("http"),
-    port = 80;
+    port = 8080,
+    gpio = require("pi-gpio");
 
 var server = http.createServer(function(request,response){
     response.writeHeader(200, {"Content-Type": "text/plain"});
