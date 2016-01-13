5dac4468f10c3b871e8c7bd28fa2c96288d85a9c
Nick Faust-Doty <n.faustdoty@gmail.com>
2016-01-12 20:46:45 -0500

    testing

--- server.js
+++ server.js
@@ -1,65 +1,42 @@
 var express = require('express'),
     app = express(),
     gpio = require('pi-gpio'),
-    pin = 40;
+    // pin = 40,
+    state;
 
 app.set('view engine', 'jade');
 
-// gpio.setup(pin, gpio.DIR_OUT);
+gpio.read(40, function(err, value) {
+    if(err) throw err;
+    state = value;
+    console.log(value);
+});
 
 app.use(express.static('public'));
 
 app.get('/', function(req, res) {
-    res.render('index', {state: "test"});
+    res.render('index', {state: state});
 });
 
 var bool = true,
     timeout;
 
 app.get('/raise', function(req, res) {
-    clearInterval(timeout);
-
-    gpio.open(pin, "output", function(err) {
-        gpio.write(pin, 1, function() {
-            gpio.close(pin);
-        });
-    });
-
-    // togglePin(false);
-    console.log('Pin #' + pin + ' turned off');
-
-    res.render('index', {state: "test"});
+    togglePin(40, 0, res);
 });
 
 app.get('/lower', function(req, res) {
-    clearInterval(timeout);
-
-    // togglePin(true);
-    console.log('Pin #' + pin + ' turned on');
-
-    res.render('index', {state: "test"});
+    togglePin(40, 1, res);
 });
 
-function togglePin(val) {
+function togglePin(pin, val, res) {
     gpio.write(pin, val, function(err) {
         if (err) throw err;
+
+        console.log('Pin ' + pin + ' set to ' + value);
+
+        res.render('index', {state: "test"});
     });
-}
-
-function tone(note) {
-    var bool = true;
-    var interval = 500 / note;
-
-    clearInterval(timeout);
-
-    timeout = setInterval(function() {
-        togglePin(bool)
-        bool = !bool;
-    }, interval);
-
-    setTimeout(function() {
-        clearInterval(timeout);
-    }, 250);
 }
 
 var server = app.listen(3000, function() {
