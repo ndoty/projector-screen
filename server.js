8c093ac2f0c8cc25919777b0d7271ba893e28e69
Nick Faust-Doty <n.faustdoty@gmail.com>
2016-01-14 19:35:17 -0500

    testing

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ var express = require('express'),
     app = express(),
     server = require('http').Server(app),
     io = require('socket.io')(server),
+    fs = require('fs'),
     gpio = require('pi-gpio'),
     pins = {
         stepPin: {
