2557babc04c34b52893e33601001a2477c0b3d6c
Nick Faust-Doty <n.faustdoty@gmail.com>
2016-01-14 19:40:44 -0500

    testing

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
-var app = require('express')(),
+var express = require('express'),
+    app = express(),
     server = require('http').Server(app),
     io = require('socket.io')(server),
     fs = require('fs'),
